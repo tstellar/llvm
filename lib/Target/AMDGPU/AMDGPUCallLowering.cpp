523bb62964e3e47e92fd2f2894a851cdedf62918
Tom Stellard <tstellar@redhat.com>
2017-06-01 18:46:52 +0000

    XXX? Remove dbg prints

--- lib/Target/AMDGPU/AMDGPUCallLowering.cpp
+++ lib/Target/AMDGPU/AMDGPUCallLowering.cpp
@@ -144,7 +144,6 @@ bool AMDGPUCallLowering::lowerFormalArguments(MachineIRBuilder &MIRBuilder,
   for (unsigned i = 0; i != NumArgs; ++i, ++CurOrigArg) {
     EVT ValEVT = TLI.getValueType(DL, CurOrigArg->getType());
 
-    dbgs() << *CurOrigArg << '\n';
     // We can only hanlde simple value types at the moment.
     ISD::ArgFlagsTy Flags;
     ArgInfo OrigArg{VRegs[i], CurOrigArg->getType()};
@@ -192,7 +191,6 @@ bool AMDGPUCallLowering::lowerFormalArguments(MachineIRBuilder &MIRBuilder,
     }
   }
 
-  dbgs() << "Add live ins \n";
   Function::const_arg_iterator Arg = F.arg_begin();
 
   if (F.getCallingConv() == CallingConv::AMDGPU_VS ||
