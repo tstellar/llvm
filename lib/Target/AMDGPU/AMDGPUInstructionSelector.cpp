64bb12e0790fb817e54a04f2f63d3f2ff233dcaf
Tom Stellard <tstellar@redhat.com>
2017-06-07 14:47:01 +0000

    AMDGPU/GlobalISel: Implement select() for 32-bit G_FADD

--- lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
+++ lib/Target/AMDGPU/AMDGPUInstructionSelector.cpp
@@ -695,6 +695,10 @@ unsigned AMDGPUInstructionSelector::getVALUOpcode(const MachineInstr &I) const {
     if (Size0 == 32)
       return AMDGPU::V_AND_B32_e64;
     break;
+  case TargetOpcode::G_FADD:
+    if (Size0 == 32)
+      return AMDGPU::V_ADD_F32_e64;
+    break;
   case TargetOpcode::G_FPTOUI:
     if (Size0 == 32 &&
         RBI.getSizeInBits(I.getOperand(1).getReg(), MRI, TRI) == 32)
@@ -759,6 +763,7 @@ bool AMDGPUInstructionSelector::select(MachineInstr &I) const {
   default:
     break;
   case TargetOpcode::G_AND:
+  case TargetOpcode::G_FADD:
   case TargetOpcode::G_FPTOUI:
   case TargetOpcode::G_OR:
   case TargetOpcode::G_SHL:
