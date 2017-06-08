00d24257efd55912d81a860c68e7bc132ff8fd06
Tom Stellard <tstellar@redhat.com>
2017-06-08 00:42:54 +0000

    AMDGPU/GlobalISel: Define instruction mapping for G_FPTOSI

--- lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -316,6 +316,7 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     // Fall-through
 
   case AMDGPU::G_FADD:
+  case AMDGPU::G_FPTOSI:
   case AMDGPU::G_FPTOUI:
   case AMDGPU::G_FMUL:
     return getDefaultMappingVOP(MI);
